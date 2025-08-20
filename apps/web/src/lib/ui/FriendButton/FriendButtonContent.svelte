90622dd5966d2bd135bd6fc74d759e8595bd9acd
Mathieu Rossi <mathieu-rossi.pro@proton.me>
2025-08-20 02:10:32 +0200

    fix: about me truncation

--- apps/web/src/lib/ui/FriendButton/FriendButtonContent.svelte
+++ apps/web/src/lib/ui/FriendButton/FriendButtonContent.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
+	import { generateHTML } from '@tiptap/core';
+	import { MESSAGE_EXTENSIONS } from 'ui/RichInput/richInputConfig';
+
 	interface Props {
 		avatar?: string;
 		display_name?: string;
-		about_me?: string;
+		about_me?: any;
 		accepted: boolean;
 		sender: boolean;
 	}
@@ -13,12 +16,32 @@
 <figure class="h-10 w-10 rounded-lg overflow-hidden">
 	<img src={avatar} class="h-full w-full object-cover" alt="" />
 </figure>
-<p class="font-medium">
-	{display_name}
-	{#if !accepted && sender}
-		<span class="text-sm text-main-500">- Pending...</span>
+<div class="flex flex-col">
+	<p class="font-medium">
+		{display_name}
+		{#if !accepted && sender}
+			<span class="text-sm text-main-500">- Pending...</span>
+		{/if}
+	</p>
+	{#if about_me}
+		<div class="text-sm text-main-500 w-[8rem] bio-truncate">
+			{@html generateHTML(about_me, MESSAGE_EXTENSIONS)}
+		</div>
 	{/if}
-</p>
-{#if about_me}
-	<p class="text-sm text-main-500">{about_me}</p>
-{/if}
+</div>
+
+<style>
+	.bio-truncate {
+		display: -webkit-box;
+		line-clamp: 1;
+		-webkit-line-clamp: 1;
+		-webkit-box-orient: vertical;
+		overflow: hidden;
+		text-overflow: ellipsis;
+	}
+
+	.bio-truncate :global(*) {
+		display: inline !important;
+		line-height: inherit;
+	}
+</style>
